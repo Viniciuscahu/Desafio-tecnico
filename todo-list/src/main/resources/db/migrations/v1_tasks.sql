aa7543ddebfe50e2cd444a350f62c254ea5a862f
Vinicius Tavares <vinicius.tavares@meilockti.com.br>
2025-02-07 03:16:49 -0300

    fix: ajust yml

--- todo-list/src/main/resources/db/migrations/v1_tasks.sql
+++ todo-list/src/main/resources/db/migrations/v1_tasks.sql
@@ -5,3 +5,34 @@ CREATE TABLE tasks (
                        created_at DATETIME NOT NULL DEFAULT GETDATE(),
                        status VARCHAR(20) NOT NULL DEFAULT 'PENDING'
 );
+
+
+-- Criar tabela no schema dev
+CREATE TABLE dev.tasks (
+                           id BIGINT PRIMARY KEY IDENTITY(1,1),
+                           title VARCHAR(255) NOT NULL,
+                           description TEXT NOT NULL,
+                           created_at DATETIME NOT NULL DEFAULT GETDATE(),
+                           status VARCHAR(20) NOT NULL DEFAULT 'PENDING'
+);
+GO
+
+-- Criar tabela no schema tst
+CREATE TABLE tst.tasks (
+                           id BIGINT PRIMARY KEY IDENTITY(1,1),
+                           title VARCHAR(255) NOT NULL,
+                           description TEXT NOT NULL,
+                           created_at DATETIME NOT NULL DEFAULT GETDATE(),
+                           status VARCHAR(20) NOT NULL DEFAULT 'PENDING'
+);
+GO
+
+-- Criar tabela no schema prd
+CREATE TABLE prd.tasks (
+                           id BIGINT PRIMARY KEY IDENTITY(1,1),
+                           title VARCHAR(255) NOT NULL,
+                           description TEXT NOT NULL,
+                           created_at DATETIME NOT NULL DEFAULT GETDATE(),
+                           status VARCHAR(20) NOT NULL DEFAULT 'PENDING'
+);
+GO
